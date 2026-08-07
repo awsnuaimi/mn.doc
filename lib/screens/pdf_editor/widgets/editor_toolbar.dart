c06d96d7286b69ef18d655c1b67d4fdb8c6f17aa
awsnuaimi <aws.nuaimi@gmail.com>
2026-08-07 16:56:23 +0200

    update

--- lib/screens/pdf_editor/widgets/editor_toolbar.dart
+++ lib/screens/pdf_editor/widgets/editor_toolbar.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
 class EditorToolbar extends StatelessWidget {
+  final Widget child;
+
   const EditorToolbar({
     super.key,
     required this.child,
   });
 
-  final Widget child;
-
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+      color: Theme.of(context).scaffoldBackgroundColor,
+      padding: const EdgeInsets.symmetric(
+        horizontal: 8,
+        vertical: 6,
+      ),
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: child,
